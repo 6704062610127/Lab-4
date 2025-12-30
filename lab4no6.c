a9d4403f8e787d0b67882003501aaa847df4c445
6704062610127 นายนนทวัฒน์ ร่มสายหยุด <s6704062610127@email.kmutnb.ac.th>
2025-12-30 13:59:38 +0700

    display square of a character

---
+++ lab4no6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    int n;
+
+    scanf(" %c %d", &ch, &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
